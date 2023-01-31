fbb01849a37a357021684aa63275ab902983b376
Kashif-Rabbani <ch.kashirabbani@gmail.com>
2023-01-31 16:15:10 +0100

    Index View updated - tabs view implemented

--- frontend/index-view.js
+++ frontend/index-view.js
@@ -5,13 +5,10 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 import { LitElement, html, css, customElement } from 'lit-element';
-import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
+import '@vaadin/tabsheet/src/vaadin-tabsheet.js';
 import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
-import '@vaadin/text-field/src/vaadin-text-field.js';
-import '@vaadin/select/src/vaadin-select.js';
-import '@vaadin/list-box/src/vaadin-list-box.js';
-import '@vaadin/item/src/vaadin-item.js';
-import '@vaadin/button/src/vaadin-button.js';
+import '@vaadin/vertical-layout/src/vaadin-vertical-layout.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
 let IndexView = class IndexView extends LitElement {
     static get styles() {
         return css `
@@ -23,57 +20,28 @@ let IndexView = class IndexView extends LitElement {
     }
     render() {
         return html `
-<vaadin-vertical-layout style="width: 100%; height: 100%; align-items: center; justify-content: center;">
- <vaadin-horizontal-layout class="header" style="width: 100%; flex-basis: var(--lumo-size-l); flex-shrink: 1; align-items: stretch; align-self: center;" id="header">
-  <h3 id="title" style="align-self: center; flex-grow: 1; padding: var(--lumo-space-s); flex-shrink: 1; text-align:center">SHACTOR : SHapes ExtrACTOR from very large Knowledge Graphs</h3>
+<vaadin-vertical-layout id="contentVerticalLayout">
+ <vaadin-horizontal-layout class="header" style="width: 100%; flex-basis: var(--lumo-size-l); flex-shrink: 0; flex-grow: 0;" id="header">
+  <h3 id="title" style="align-self: stretch; flex-grow: 1; flex-shrink: 1; text-align:center;">SHACTOR : SHapes ExtrACTOR from very large Knowledge Graphs</h3>
  </vaadin-horizontal-layout>
- <vaadin-vertical-layout class="content" style="width: 100%; flex-grow: 1; flex-shrink: 1; flex-basis: auto;">
-  <br>
-  <h6 style="align-self: center; margin: var(--lumo-space-s); margin-left: var(--lumo-space-m); width: 50%;">SHACTOR is a tool which helps extracting SHACL shapes from very large Knowledge Graphs. Please select one of the following options.</h6>
-  <br>
-  <vaadin-select value="Item one" id="vaadinSelect" style="align-self: flex-start; width: 50%; margin-left: 25%;" label="Select from existing datasets">
-   <template>
-    <vaadin-list-box>
-     <vaadin-item>
-       Item one 
-     </vaadin-item>
-     <vaadin-item>
-       Item two 
-     </vaadin-item>
-     <vaadin-item>
-       Item three 
-     </vaadin-item>
-    </vaadin-list-box>
-   </template>
-  </vaadin-select>
-  <br>
-  <vaadin-button theme="primary" id="continueButton" style="margin-left: 25%; align-self: flex-start;" tabindex="0">
-    Continue 
-  </vaadin-button>
-  <br>
-  <vaadin-text-field label="Enter Graph URL (in .NT format)" id="graphUrl" style="align-self: center; flex-grow: 0; margin: var(--lumo-space-s); padding: var(--lumo-space-m); flex-shrink: 0; width: 50%;" type="text" name="graphUrl">
-    Text 
-  </vaadin-text-field>
-  <vaadin-button theme="primary" id="uploadGraphButton" style="align-self: flex-start; margin-left: 25%;" tabindex="0">
-    Upload Graph 
-  </vaadin-button>
-  <h6 style="align-self: flex-start; flex-grow: 0; margin-left: 25%;">Or, if you want to connect to a SPARQL endpoint</h6>
-  <vaadin-text-field label="Enter address of a SPARQL endpoint" id="graphEndpointUrl" style="align-self: center; flex-grow: 0; margin: var(--lumo-space-s); padding: var(--lumo-space-m); width: 50%;" type="text" name="graphEndpointUrl">
-    Text 
-  </vaadin-text-field>
-  <vaadin-button theme="primary" id="graphEndpointButton" style="align-self: flex-start; margin-left: 25%;" tabindex="0">
-    Connect 
-  </vaadin-button>
-  <h6 style="margin-left: 25%;">Or, if you already have SHACL shapes, upload them here to analyze with SHACTOR</h6>
-  <vaadin-text-field label="Enter Shapes File URL (in .TTL format)" style="align-self: center; width: 50%; margin: var(--lumo-space-s); padding: var(--lumo-space-m);" type="text"></vaadin-text-field>
-  <vaadin-button theme="primary" style="margin-left: 25%;" tabindex="0">
-   Upload Shapes
-  </vaadin-button>
+ <vaadin-vertical-layout style="align-self: center;">
+  <h6 style="align-self: center; width: 80%;">About SHACTOR:</h6>
+  <p style="align-self: center; width: 80%;">SHACTOR is a system for extracting quality SHACL shapes from very large Knowledge Graphs (KGs), analyzing them to find spurious shapes constraints, and finding erroneous triples in the KG. The SHACL shapes represent prominent data patterns within KG but are likely to contain some spurious constraints extracted due to the presence of erroneous data in the KG. Given a KG having millions of triples and thousands of classes, SHACTOR parses the KG using our efficient and scalable shapes extraction algorithm and outputs SHACL shapes constraints. Further, it uses the concepts of support and confidence to prune the spurious shape constraints. You can use SHACTOR to extract, analyze, and clean SHACL shape constraints from very large KGs, it helps you find and correct errors in the KG by automatically generating SPARQL queries for your KG. SHACTOR will take you through different steps of shapes extraction and show information about the graph. Please begin by starting the process of shapes extraction:</p>
+  <vaadin-tabsheet id="tabSheet" style="flex-grow: 0; align-self: stretch; width: 100%; flex-shrink: 0; margin: var(--lumo-space-xl);"></vaadin-tabsheet>
  </vaadin-vertical-layout>
- <vaadin-horizontal-layout class="footer" style="width: 100%; flex-basis: var(--lumo-size-l); flex-shrink: 0; background-color: var(--lumo-contrast-10pct);">
-  <h6 style="align-self: center; margin: var(--lumo-space-l);">Credits: Kashif Rabbani, Matteo Lissandrini, Katja Hose</h6>
-  <h6 id="footer_credits_uni" style="align-self: center; margin: var(--lumo-space-l); flex-grow: 1; padding: var(--lumo-space-m);"> Aalborg University, Denmark</h6>
- </vaadin-horizontal-layout>
+ <vaadin-vertical-layout theme="spacing" style="width: 100%; height: 100%;">
+  <div id="footer" style="flex-grow: 0; align-self: stretch; flex-shrink: 0;">
+   <div id="footerImageLeftDiv">
+    <img id="footerLeftImage">
+   </div>
+   <div id="footerAboutDiv">
+    <p id="footerAboutParagraph">Authors: Kashif Rabbani, Matteo Lissandrini, and Katja Hose</p>
+   </div>
+   <div id="footerImageRightDiv">
+    <img id="footerRightImage">
+   </div>
+  </div>
+ </vaadin-vertical-layout>
 </vaadin-vertical-layout>
 `;
     }
--- src/main/java/shactor/IndexView.java
+++ src/main/java/shactor/IndexView.java
@@ -1,11 +1,19 @@
 package shactor;
 
 import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.littemplate.LitTemplate;
 import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.select.Select;
+import com.vaadin.flow.component.tabs.TabSheet;
+import com.vaadin.flow.component.tabs.TabSheetVariant;
 import com.vaadin.flow.component.template.Id;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
@@ -14,44 +22,121 @@ import com.vaadin.flow.router.Route;
 @JsModule("./index-view.ts")
 @Route("")
 public class IndexView extends LitTemplate {
-    
-    @Id("graphUrl")
-    private TextField graphUrl;
-    @Id("uploadGraphButton")
-    private Button uploadGraphButton;
-    @Id("graphEndpointUrl")
-    private TextField graphEndpointUrl;
-    @Id("graphEndpointButton")
-    private Button graphEndpointButton;
-    
     public static String graphURL = "";
-    @Id("vaadinSelect")
-    private Select<String> vaadinSelect;
+
+    @Id("tabSheet")
+    private TabSheet tabSheet;
+    @Id("footer")
+    private Div footer;
+    @Id("footerImageLeftDiv")
+    private Div footerImageLeftDiv;
+    @Id("footerImageRightDiv")
+    private Div footerImageRightDiv;
+    @Id("footerRightImage")
+    private Image footerRightImage;
+    @Id("footerLeftImage")
+    private Image footerLeftImage;
     
-    @Id("continueButton")
-    private Button continueButton;
     
     public IndexView() {
-        
-        vaadinSelect.setItems("DBpedia", "LUBM", "YAGO-4", "WikiData");
-        vaadinSelect.setValue("DBpedia");
-        
-        continueButton.addClickListener(buttonClickEvent -> {
-            //graphURL = "/Users/kashifrabbani/Documents/GitHub/data/CityDBpedia.nt";
-            // Server: /home/ubuntu/data/dbpedia_ml.nt
-            graphURL = "/home/ubuntu/data/dbpedia_ml.nt";
+        footerLeftImage.setSrc("./images/DKW-Logo.png");
+        footerRightImage.setSrc("./images/aau.png");
+        tabSheet.addThemeVariants(TabSheetVariant.LUMO_TABS_EQUAL_WIDTH_TABS);
+        tabSheet.add("Use Existing Datasets", getTabOneLayout());
+        tabSheet.add("Upload Graph", getTabTwoLayout());
+        tabSheet.add("Connect to SPARQL Endpoint", getTabThreeLayout());
+        tabSheet.add("Analyze SHACL Shapes", getTabFourLayout());
+    }
     
+    private VerticalLayout getTabOneLayout(){
+        VerticalLayout vl = getVerticalLayout();
+        vl.add(configureAndGetSelectField());
+        Button continueButton = getPrimaryButton("Continue");
+        continueButton.addClickListener(buttonClickEvent -> {
+            graphURL = "/Users/kashifrabbani/Documents/GitHub/data/CityDBpedia.nt";
+            // Server: /home/ubuntu/data/dbpedia_ml.nt
+            //graphURL = "/home/ubuntu/data/dbpedia_ml.nt";
             continueButton.getUI().ifPresent(ui -> ui.navigate("selection-view"));
         });
+        vl.add(continueButton);
+        return vl;
+    }
+    
+    private VerticalLayout getTabTwoLayout(){
+        VerticalLayout vl = getVerticalLayout();
+        TextField textField = getTextField("Enter Graph URL (in .NT Format)");
         
+        Button uploadGraphButton = getPrimaryButton("Upload Graph");
+        vl.add(textField);
+        vl.add(uploadGraphButton);
+    
         uploadGraphButton.addClickListener(buttonClickEvent -> {
-            Notification.show( "URL: " + graphUrl.getValue());
-            graphURL = graphUrl.getValue();
-            graphEndpointButton.getUI().ifPresent(ui -> ui.navigate("selection-view"));
+            Notification.show( "URL: " + textField.getValue());
+            graphURL = textField.getValue();
+            uploadGraphButton.getUI().ifPresent(ui -> ui.navigate("selection-view"));
         });
+        return vl;
+    }
+    
+    private VerticalLayout getTabThreeLayout(){
+        VerticalLayout vl = getVerticalLayout();
+        TextField textField = getTextField("Enter address of a SPARQL endpoint");
+        Button graphEndpointButton = getPrimaryButton("Connect");
+        vl.add(textField);
+        vl.add(graphEndpointButton);
     
         graphEndpointButton.addClickListener(buttonClickEvent -> {
-            graphEndpointButton.getUI().ifPresent(ui -> ui.navigate("selection-view"));
+            graphURL = textField.getValue();
+            Notification.show( "Not Implemented Yet!" );
+            //graphEndpointButton.getUI().ifPresent(ui -> ui.navigate("selection-view"));
         });
+        return vl;
     }
+    private VerticalLayout getTabFourLayout(){
+        VerticalLayout vl = getVerticalLayout();
+        TextField textField = getTextField("Enter Shapes File URL (in .TTL format)");
+        
+        Button shapesUploadButton = getPrimaryButton("Upload");
+        vl.add(textField);
+        vl.add(shapesUploadButton);
+    
+        shapesUploadButton.addClickListener(buttonClickEvent -> {
+            graphURL = textField.getValue();
+            Notification.show( "Not Implemented Yet!" );
+            //shapesUploadButton.getUI().ifPresent(ui -> ui.navigate("selection-view"));
+        });
+        return vl;
+    }
+    
+    // Helper Methods
+    
+    private VerticalLayout getVerticalLayout(){
+        VerticalLayout verticalLayout = new VerticalLayout();
+        verticalLayout.setSizeFull();
+        verticalLayout.setAlignItems(FlexComponent.Alignment.CENTER);
+        return verticalLayout;
+    }
+    
+    private TextField getTextField(String label){
+        TextField textField = new TextField();
+        textField.setWidth("50%");
+        textField.setLabel(label);
+        return textField;
+    }
+    
+    private Select<String> configureAndGetSelectField(){
+        Select<String> selectField = new Select<>();
+        selectField.setWidth("50%");
+        selectField.setLabel("Select from existing datasets");
+        selectField.setItems("DBpedia", "LUBM", "YAGO-4", "WikiData");
+        selectField.setValue("DBpedia");
+        return selectField;
+    }
+    
+    private Button getPrimaryButton(String label) {
+        Button primaryButton = new Button(label);
+        primaryButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        return primaryButton;
+    }
+    
 }
